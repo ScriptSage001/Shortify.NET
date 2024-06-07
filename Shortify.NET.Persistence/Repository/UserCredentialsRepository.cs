b97cb6d45cb6ab27482014db4aaebbd291726508
Kaustab Samanta <scriptsage001@gmail.com>
2024-06-08 01:37:13 +0530

    Optimized UserCredentials Repository

--- Shortify.NET.Persistence/Repository/UserCredentialsRepository.cs
+++ Shortify.NET.Persistence/Repository/UserCredentialsRepository.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Shortify.NET.Applicaion.Abstractions.Repositories;
 using Shortify.NET.Core.Entites;
 using Shortify.NET.Core.ValueObjects;
-
+using System.Linq.Expressions;
 namespace Shortify.NET.Persistence.Repository
 {
     public class UserCredentialsRepository : IUserCredentialsRepository
@@ -12,13 +13,65 @@ namespace Shortify.NET.Persistence.Repository
         public UserCredentialsRepository(AppDbContext appDbContext)
             => _appDbContext = appDbContext;
 
-        public async Task<UserCredentials?> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default)
+        #region Private Methods
+
+        /// <summary>
+        /// Common Method to Reduce Repetitive Querying Logic
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="includeExpression"></param>
+        /// <param name="asNoTracking"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private async Task<UserCredentials?> GetUserCredentialsAsync(
+            Expression<Func<UserCredentials, bool>> predicate,
+            Expression<Func<UserCredentials, Object>>? includeExpression = null,
+            bool asNoTracking = false,
+            CancellationToken cancellationToken = default)
         {
-            return await _appDbContext
-                            .Set<UserCredentials>()
-                            .Where(creds => creds.UserId == userId)
+            IQueryable<UserCredentials> query = _appDbContext.Set<UserCredentials>();
+
+            if (asNoTracking)
+            {
+                query.AsNoTracking();
+            }
+
+            if (includeExpression is not null)
+            {
+                query = query.Include(includeExpression);
+            }
+
+            return await query.FirstOrDefaultAsync(predicate, cancellationToken);
+        }
+
+        /// <summary>
+        /// Common Method to Reduce Repetitive Querying Logic of GetUserCredentials From User
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="asNoTracking"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private async Task<UserCredentials?> GetUserCredentialsFromUserAsync(
+            Expression<Func<User, bool>> predicate,
+            bool asNoTracking = false,
+            CancellationToken cancellationToken = default)
+        {
+            IQueryable<User> query = _appDbContext.Set<User>();
+            
+            if (asNoTracking)
+            {
+                query.AsNoTracking();
+            }
+
+            return await query
+                            .Where(predicate)
+                            .Select(user => user.UserCredentials)
                             .FirstOrDefaultAsync(cancellationToken);
         }
+
+        #endregion
+
+        #region Public Region
 
         public void Add(UserCredentials userCredentials)
         {
@@ -35,34 +88,24 @@ namespace Shortify.NET.Persistence.Repository
             _appDbContext.Set<UserCredentials>().Remove(userCredentials);
         }
 
+        public async Task<UserCredentials?> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default)
+            => await GetUserCredentialsAsync(
+                    userCreds => userCreds.UserId == userId,
+                    asNoTracking: true,
+                    cancellationToken: cancellationToken);
+
         public async Task<UserCredentials?> GetByUserNameAsync(UserName userName, CancellationToken cancellationToken = default)
-        {
-            return await _appDbContext
-                            .Set<UserCredentials>()
-                            .Join(
-                                _appDbContext.Set<User>(),
-                                userCreds => userCreds.UserId,
-                                user => user.Id,
-                                (userCreds, user) => new {userCreds, user.UserName})
-                            .Where(
-                                 u => u.UserName == userName)
-                            .Select(u => u.userCreds)
-                            .FirstOrDefaultAsync(cancellationToken);
-        }
+            => await GetUserCredentialsFromUserAsync(
+                        user => user.UserName == userName,
+                        true,
+                        cancellationToken);
 
         public async Task<UserCredentials?> GetByEmailAsync(Email email, CancellationToken cancellationToken = default)
-        {
-            return await _appDbContext
-                            .Set<UserCredentials>()
-                            .Join(
-                                _appDbContext.Set<User>(),
-                                userCreds => userCreds.UserId,
-                                user => user.Id,
-                                (userCreds, user) => new { userCreds, user.Email })
-                            .Where(
-                                 u => u.Email == email)
-                            .Select(u => u.userCreds)
-                            .FirstOrDefaultAsync(cancellationToken);
-        }
+            => await GetUserCredentialsFromUserAsync(
+                        user => user.Email == email,
+                        true,
+                        cancellationToken);
+
+        #endregion
     }
 }
